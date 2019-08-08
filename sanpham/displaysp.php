95d7b762761bb068e228611b31bc9176cd1a16c1
queanh8498 <anhb1606867@student.ctu.edu.vn>
2019-08-08 19:27:07 +0700

    commit ne

--- sanpham/displaysp.php
+++ sanpham/displaysp.php
@@ -1,7 +1,16 @@
 day la display san pham 
 <?php 
     require_once __DIR__ . "/../dbconnect.php";
-    $sql = "select * from `sanpham`;";
+    //HERE DOCS
+    $sql = <<<EOT
+    SELECT sp.sp_ma,sp.sp_ten, sp.sp_gia, sp.sp_giacu, sp.sp_soluong, lsp.lsp_ten, nsx.nsx_ten, km.km_ten
+    FROM sanpham sp
+    JOIN loaisanpham lsp ON sp.lsp_ma=lsp.lsp_ma
+    JOIN nhasanxuat nsx ON sp.nsx_ma=nsx.nsx_ma
+    LEFT JOIN khuyenmai km ON km.km_ma=sp.km_ma ;    
+    
+EOT;
+
     $rs = mysqli_query($conn, $sql);
 
     $data = [];
@@ -10,11 +19,14 @@ day la display san pham
         'sp_ma' => $row['sp_ma'],
         'sp_ten' => $row['sp_ten'],
         'sp_gia' => $row['sp_gia'],
-        'sp_ngaycapnhat' => $row['sp_ngaycapnhat'],
+        'sp_giacu' => $row['sp_giacu'],
         'sp_soluong' => $row['sp_soluong'],
+        'lsp_ten' => $row['lsp_ten'],
+        'nsx_ten' => $row['nsx_ten'],
+        'km_ten' => $row['km_ten'],
     );
 }
-    //print_r($data);
+    //print_r($data); 
     //die;
 ?>
 
@@ -23,18 +35,27 @@ day la display san pham
     <th>Mã </th>
     <th>Tên SP</th>
     <th>Gía</th>
-    <th>Ngày cập nhật</th>
+    <th>Gía cu</th>
     <th>Số lượng </th>
+    <th>Ten LSP</th>
+    <th>NSX</th>
+    <th>KHUYEN MAI</th>
+    
 </tr>
     <?php foreach ($data as $row) : ?>
     <tr>
         <td><?php echo $row['sp_ma'];?></td>
         <td><?php echo $row['sp_ten'];?></td>
         <td><?php echo $row['sp_gia'];?></td>
-        <td><?php echo $row['sp_ngaycapnhat'];?></td>
+        <td><?php echo $row['sp_giacu'];?></td>
         <td><?php echo $row['sp_soluong'];?></td>
-        <td><a href="/sephora/sanpham/updatesp.php?sp_ma=<?php $row['sp_ma']; ?>">Sửa</a></td>
-        <td><a href="/sephora/sanpham/deletesp.php?sp_ma=<?php $row['sp_ma']; ?>">Xóa</a></td>
+        <td><?php echo $row['lsp_ten'];?></td>
+        <td><?php echo $row['nsx_ten'];?></td>
+        <td><?php echo $row['km_ten'];?></td>
+        
+        <!--<td><a href="/sephora/sanpham/updatesp.php?sp_ma=<?php $row['sp_ma']; ?>">Sửa</a></td>
+        <td><a href="/sephora/sanpham/deletesp.php?sp_ma=<?php $row['sp_ma']; ?>">Xóa</a></td>-->
+        
     </tr>
 <?php endforeach; ?>
 
---
+++ sanpham/insertsp.php
@@ -0,0 +1,63 @@
+<?php 
+
+require_once __DIR__ . "/../dbconnect.php";
+    //HERE DOCS
+    $sql = <<<EOT
+    SELECT * FROM loaisanpham;
+EOT;
+
+    
+$rs = mysqli_query($conn, $sql);
+
+$data = [];
+while ($row = mysqli_fetch_array($rs, MYSQLI_ASSOC)) {
+    $data[] = array(
+    'lsp_ma' => $row['lsp_ma'],
+    'lsp_ten' => $row['lsp_ten'],
+);
+}
+
+    $sqlNSX = <<<EOT
+    SELECT * FROM nhasanxuat;
+EOT;
+
+    $rsNSX = mysqli_query($conn, $sqlNSX);
+
+    $dataNSX = [];
+    while ($rowNSX = mysqli_fetch_array($rsNSX, MYSQLI_ASSOC)) {
+    $dataNSX[] = array(
+    'nsx_ma' => $rowNSX['nsx_ma'],
+    'nsx_ten' => $rowNSX['nsx_ten'],
+);
+}
+
+
+//print_r($data); 
+//die;
+?>
+
+
+
+<form name="frmInsertSP" id="frmInsertSP" method="post" action="">
+    Ten SP: <input type="text" name="sp_ten" id="sp_ten" > <br>
+    Gia SP<input type="text" name="sp_gia" id="sp_gia" > <br>
+    Gia cu SP: <input type="text" name="sp_giacu" id="sp_giacu" > <br>
+    Ngay cap nhat: <input type="text" name="sp_ngaycapnhat" id="sp_ngaycapnhat" > <br>
+    So luong: <input type="text" name="sp_soluong" id="sp_soluong" > <br>
+    Loai SP: 
+    <select name="lsp_ten" id="lsp_ten"> 
+    <?php foreach ($data as $row) : ?> 
+        <option value="<?php echo $row['lsp_ma']; ?>"><?php echo $row['lsp_ten']; ?></option><br>
+    <?php endforeach; ?>
+    </select>
+
+    NSX: <select name="nsx_ten" id="nsx_ten"> 
+    <?php foreach ($dataNSX as $rowNSX) : ?> 
+        <option value="<?php echo $rowNSX['nsx_ma']; ?>"><?php echo $rowNSX['nsx_ten']; ?></option><br>
+    <?php endforeach; ?>
+    </select>
+    Khuyen mai: <br>
+    <input type="submit" name="submitSave" id="submitSave" value="SAVE">
+
+
+</form>
